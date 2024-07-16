0c629bdf204ee5d4f61ba4ad197eb4e1bc041c02
maahin2005 <mahin.malek2005@gmail.com>
2024-07-16 12:30:58 +0530

    DecreasingSpaceComplexityOfFeb

--- DP/fibonacci.py
+++ DP/fibonacci.py
@@ -1,4 +1,4 @@
-n = 1
+n = 4
 dp = [-1] * (n+1)
 def f(n,dp):
     if n <= 1:
@@ -11,9 +11,12 @@ def f(n,dp):
 
     return dp[n]
 
+# TC: O(N) 
+# SC: O(N) + call stack O(N)
 print(f"{n}th fib is {f(n,dp)}")
 
-# Bottom Up Approach:
+# Bottom Up Approach (Tabulation): 
+# Reduce the call stack from SC
 
 def solve(n):
     if n <= 1:
@@ -28,4 +31,23 @@ def solve(n):
 
     return dp[n]
 
+# TC: O(N)
+# SC: O(N)
 print(solve(n))
+
+# Again Try to Reduce the SC:
+
+def fn(n):
+    prev_2 = 0
+    prev_1 = 1
+
+    for _ in range(2,n+1):
+        currI = prev_1 + prev_2
+        prev_2 = prev_1
+        prev_1 = currI
+    
+    return prev_1
+
+# TC: O(N)
+# SC: O(1)
+print(fn(n))
