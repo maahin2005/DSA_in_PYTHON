8b45e1cf746163cc73da03c9b370215d8af0a87c
maahin2005 <mahin.malek2005@gmail.com>
2024-07-16 12:14:50 +0530

    FinWithTabulation( ):DP

--- DP/fibonacci.py
+++ DP/fibonacci.py
@@ -1,4 +1,4 @@
-n = 4
+n = 1
 dp = [-1] * (n+1)
 def f(n,dp):
     if n <= 1:
@@ -12,3 +12,20 @@ def f(n,dp):
     return dp[n]
 
 print(f"{n}th fib is {f(n,dp)}")
+
+# Bottom Up Approach:
+
+def solve(n):
+    if n <= 1:
+        return n
+
+    dp=[0]*(n+1)
+
+    dp[1]=1
+
+    for i in range(2,n+1):
+        dp[i] = dp[i-1]+dp[i-2]
+
+    return dp[n]
+
+print(solve(n))
