3c06dfd04ebe479543e92a58a10766b23346428d
maahin2005 <mahin.malek2005@gmail.com>
2024-08-20 19:12:45 +0530

    Quick sort SC

--- Sorting_Algorithms/quick.py
+++ Sorting_Algorithms/quick.py
@@ -1,6 +1,7 @@
 arr = [-5, 3, 2, 1, 0, -2, -3, -3, 7, 5, 2]
 
 # Time Complexity: O(N log N) - Average Case || O(N^2) - worst case (if Pivot is useless)
+
 # Space Complexity: O(N)
 
 def quick_sort(arr):
